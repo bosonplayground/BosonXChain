7c06130238751768049d9564e3831990550e8c76
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2021-05-06 15:24:16 +0100

    add configs for skale and goerli networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,12 +13,12 @@ if (!process.env.MNEMONIC) {
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async() => {
+    const accounts = await ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 // You need to export an object to set up your config
@@ -28,49 +28,70 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: {
-    version: "0.7.1",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 10
-      }
+    solidity: {
+        version: "0.7.1",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 10
+            }
+        }
+    },
+    networks: {
+        ganache: {
+            chainId: 1337,
+            url: "http://127.0.0.1:8545",
+            accounts: {
+                count: 10,
+                initialIndex: 0,
+                mnemonic: process.env.MNEMONIC,
+                path: "m/44'/60'/0'/0",
+            },
+        },
+        mumbai: {
+            chainId: 80001,
+            url: `https://rpc-mumbai.matic.today`,
+            accounts: {
+                count: 10,
+                initialIndex: 0,
+                mnemonic: process.env.MNEMONIC,
+                path: "m/44'/60'/0'/0",
+            },
+        },
+        arbitrumTestnet: {
+            chainId: 212984383488152,
+            url: 'https://kovan4.arbitrum.io/rpc',
+            accounts: {
+                count: 10,
+                initialIndex: 0,
+                mnemonic: process.env.MNEMONIC,
+                path: "m/44'/60'/0'/0",
+            },
+            gasPrice: 0,
+        },
+        skale: {
+            chainId: 1533527623516183,
+            url: 'https://eth-global-5.skalenodes.com:10008',
+            accounts: {
+                count: 10,
+                initialIndex: 0,
+                mnemonic: process.env.MNEMONIC,
+                path: "m/44'/60'/0'/0",
+            },
+            gasPrice: 0,
+        },
+        goerli: {
+            chainId: 5,
+            url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            accounts: {
+                count: 10,
+                initialIndex: 0,
+                mnemonic: process.env.MNEMONIC,
+                path: "m/44'/60'/0'/0",
+            },
+        },
+    },
+    mocha: {
+        timeout: 20000
     }
-  },
-  networks: {
-    ganache: {
-      chainId: 1337,
-      url: "http://127.0.0.1:8545",
-      accounts: {
-          count: 10,
-          initialIndex: 0,
-          mnemonic: process.env.MNEMONIC,
-          path: "m/44'/60'/0'/0",
-      },
-    },
-    mumbai: {
-      chainId: 80001,
-      url: `https://rpc-mumbai.matic.today`,
-      accounts: {
-          count: 10,
-          initialIndex: 0,
-          mnemonic: process.env.MNEMONIC,
-          path: "m/44'/60'/0'/0",
-      },
-    },
-    arbitrumTestnet: {
-      url: 'https://kovan4.arbitrum.io/rpc',
-      accounts: {
-        count: 10,
-        initialIndex: 0,
-        mnemonic: process.env.MNEMONIC,
-        path: "m/44'/60'/0'/0",
-    },
-      gasPrice: 0,
-    },
-  },
-  mocha: {
-    timeout: 20000
-  }
 };
-
